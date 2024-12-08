f68d915ae14852875ed431d8524a732cfe5d3364
Alex Tomjack <amt1309@gmail.com>
2024-12-08 15:24:03 -0600

    Small syntax fix

--- voice-server/server.py
+++ voice-server/server.py
@@ -8,6 +8,6 @@ DEFAULT_PORT = 8080
 def send_message(message):
     ws = websocket.WebSocket()
     ws.connect(
-        f"ws://localhost:{int(os.environ.get("SERVER_PORT", DEFAULT_PORT))}")
+        f"ws://localhost:{int(os.environ.get('SERVER_PORT', DEFAULT_PORT))}")
     ws.send(json.dumps(message))
     ws.close()
